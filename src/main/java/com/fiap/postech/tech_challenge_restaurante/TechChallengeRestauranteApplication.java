f4e7e5a4c9d56df59011d3b798f36163763e9d21
Renan Leite <renan.carli@hotmail.com>
2024-10-18 17:19:35 -0300

    Criando aplicação

---
+++ src/main/java/com/fiap/postech/tech_challenge_restaurante/TechChallengeRestauranteApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.postech.tech_challenge_restaurante;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechChallengeRestauranteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechChallengeRestauranteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fiap/postech/tech_challenge_restaurante/TechChallengeRestauranteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.postech.tech_challenge_restaurante;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechChallengeRestauranteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
